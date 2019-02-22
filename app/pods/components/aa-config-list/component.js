c9da1d3cfecfb9e9f9dfdd61bd263228fa0f3da4
ddallaire <ddallaire25@gmail.com>
2019-02-22 13:23:00 -0500

    Basic layout

---
+++ app/pods/components/aa-config-list/component.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import {computed} from '@ember/object';
+// import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  configurations: computed(() => {
+    return [{name: 'first'}, {name: 'second'}];
+  })
+});
