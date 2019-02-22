d062f1d6f45b48237a73ab7ee17bf6893c540055
ddallaire <ddallaire25@gmail.com>
2019-02-22 09:30:39 -0500

    Session service

---
+++ tests/unit/session-test.js
@@ -0,0 +1,67 @@
+import {expect} from 'chai';
+import {describe, it, beforeEach, afterEach} from 'mocha';
+import {setupTest} from 'ember-mocha';
+
+import config from 'adaptone-front/config/environment';
+
+describe('Unit | Services | session', () => {
+  setupTest('service:session', {
+    // needs: ['service:filesystem']
+  });
+
+  let service;
+  const configuration = {
+    name: 'test',
+    monitorCount: 2,
+    speakerCount: 8,
+    probeCount: 8
+  };
+
+  describe('persist', () => {
+    beforeEach(function() {
+      service = this.subject();
+    });
+
+    afterEach(() => {
+      localStorage.removeItem(config.APP.LOCAL_STORAGE.SESSION_NAMESPACE);
+    });
+
+    it('should persist the configuration to localStorage', () => {
+      service.set('configuration', configuration);
+
+      expect(localStorage.getItem(config.APP.LOCAL_STORAGE.SESSION_NAMESPACE)).to.equal(JSON.stringify(configuration));
+    });
+  });
+
+  describe('fetch', () => {
+    beforeEach(function() {
+      service = this.subject();
+      localStorage.setItem(config.APP.LOCAL_STORAGE.SESSION_NAMESPACE, JSON.stringify(configuration));
+    });
+
+    afterEach(() => {
+      localStorage.removeItem(config.APP.LOCAL_STORAGE.SESSION_NAMESPACE);
+    });
+
+    it('should fetch the configuration from localStorage', () => {
+      expect(service.get('configuration')).to.deep.equal(configuration);
+    });
+  });
+
+  describe('remove', () => {
+    beforeEach(function() {
+      service = this.subject();
+      localStorage.setItem(config.APP.LOCAL_STORAGE.SESSION_NAMESPACE, JSON.stringify(configuration));
+    });
+
+    afterEach(() => {
+      localStorage.removeItem(config.APP.LOCAL_STORAGE.SESSION_NAMESPACE);
+    });
+
+    it('should remove the configuration from localStorage', () => {
+      service.remove();
+
+      expect(localStorage.getItem(config.APP.LOCAL_STORAGE.SESSION_NAMESPACE)).to.be.null;
+    });
+  });
+});
