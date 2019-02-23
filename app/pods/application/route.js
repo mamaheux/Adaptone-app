6079ba06b184bfe5e714eff94036d2fa303b9f3e
David Dallaire <ddallaire25@gmail.com>
2019-02-23 09:55:03 -0500

    Basic layout (#6)

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  intl: service(),
+
+  beforeModel() {
+    return this.intl.setLocale('fr-ca');
+  }
 });
