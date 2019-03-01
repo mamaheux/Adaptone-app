8086d05254bb2c7674470b406cac18074c21f7ea
Andy <andy1337@live.ca>
2019-03-01 13:17:18 -0500

    Cleaned up code

--- app/pods/components/aa-peak-meter/component.js
+++ app/pods/components/aa-peak-meter/component.js
@@ -1,100 +1,87 @@
 import Component from '@ember/component';
 import {observer} from '@ember/object';
 
-const DEFAULT_YELLOW_LIMIT = 0.65;
-const DEFAULT_RED_LIMIT = 0.85;
-
-let Peak = function(delay, fallDuration) {
-  this.value = 0;
-  this.lastTime = 0;
-  this.falling = null;
-
-  if (!fallDuration) {
-    fallDuration = 200;
-
-    if (!delay) {
-      delay = 1000;
-    }
-  }
-
-  this.delay = delay;
-  this.fallDuration = fallDuration;
-}
+const DEFAULT_PEAK_FALL_DELAY = 2000;
+const DEFAULT_PEAK_FALL_DURATION = 500;
 
 export default Component.extend({
-  yellowLimit: DEFAULT_YELLOW_LIMIT,
-  redLimit: DEFAULT_RED_LIMIT,
-  proxyDelay: 60,
-  proxyFallDuration: 200,
-  min: 0,
-  max: 1,
-  peakWidth: 2,
+  max: null,
   meterElements: null,
+  min: null,
+  redLimit: null,
   value: null,
+  yellowLimit: null,
+
+  peakFallDelay: DEFAULT_PEAK_FALL_DELAY,
+  peakFallDuration: DEFAULT_PEAK_FALL_DURATION,
 
   valueChanged: observer('value', function() {
     this.renderPeakMeter(this.get('meterValue'));
-    return this.get('meterValue');
   }),
 
   didInsertElement() {
     this._super(...arguments);
-    this._initializeMeterElements(this.element);
+    this.initializeMeterElements(this.element);
 
     this.renderPeakMeter();
   },
 
-  renderPeakMeter(value = this.get('value')) {
-    console.log('RENDERING PEAK METERS -----------------------------------');
+  initializeMeterElements() {
+    this.set('meterElements', {
+      green: this.element.querySelector('.green'),
+      yellow: this.element.querySelector('.yellow'),
+      red: this.element.querySelector('.red'),
+      peak: this.element.querySelector('.peak'),
+      size: this.element.querySelector('.peak-meter').clientWidth,
+      peakParameters: {
+        value: 0,
+        lastTime: 0,
+        falling: null,
+        fallDuration: this.get('peakFallDuration'),
+        delay: this.get('peakFallDelay')
+      }
+    });
 
-    value = {
-      value: value,
-      peak: this._updatePeakValue(this.get('meterElements').proxy.value, value)
-    };
-
-    this.drawPeakMeter(value);
+    this.get('meterElements').yellow.style.left = this.formatValue(this.get('yellowLimit'));
+    this.get('meterElements').red.style.left = this.formatValue(this.get('redLimit'));
   },
 
-  drawPeakMeter(value) {
-    console.log('DRAWING PEAK METERS -----------------------------------');
+  renderPeakMeter(value = this.get('value')) {
+    const meterValue = {
+      currentValue: value,
+      peak: this.updatePeakValue(this.get('meterElements').peakParameters, value)
+    };
 
-    let peak = this._normalize(value.peak.value);
-    console.log(peak);
-    value = value.value;
+    this.drawPeakMeter(meterValue);
+  },
 
-    value = this._normalize(value);
+  drawPeakMeter(meterValue) {
+    let peak = this.normalize(meterValue.peak.value);
+
+    let value = meterValue.currentValue;
+
+    value = this.normalize(value);
 
     let dom = this.get('meterElements');
 
-    dom.green.style.width = this._formatValue(value);
+    dom.green.style.width = this.formatValue(value);
 
     if (value > this.get('yellowLimit')) {
-      dom.yellow.style.width = this._formatValue(value - this.get('yellowLimit'));
+      dom.yellow.style.width = this.formatValue(value - this.get('yellowLimit'));
     } else {
       dom.yellow.style.width = 0;
     }
 
     if (value > this.get('redLimit')) {
-        dom.red.style.width = this._formatValue(value - this.get('redLimit'));
+        dom.red.style.width = this.formatValue(value - this.get('redLimit'));
     } else {
         dom.red.style.width = 0;
     }
 
-    dom.peak.style.left = `${Math.round(peak * dom.size - dom.peak.style.width)}px`;
+    dom.peak.style.left = this.formatValue(peak - dom.peak.style.width);
   },
 
-  _normalize(value) {
-    if (value > this.get('max')) {
-        return this.get('max');
-    } else if (value < this.get('min') || isNaN(value)) {
-        return this.get('min');
-    } else {
-        return (value - this.get('min') / this.get('max') - this.get('min'));
-    }
-  },
-
-  _updatePeakValue(peak, value) {
-    console.log('UPDATING PEAK VALUE -----------------------------------');
+  updatePeakValue(peak, value) {
     let now = (new Date()).getTime();
 
     if (value > peak.value) {
@@ -117,25 +104,18 @@ export default Component.extend({
     return peak;
   },
 
-  _initializeMeterElements() {
-    console.log('INITIALIZING METER ELEMENTS ----------------');
-    this.set('meterElements', {
-      green: this.element.querySelector('.green'),
-      yellow: this.element.querySelector('.yellow'),
-      red: this.element.querySelector('.red'),
-      peak: this.element.querySelector('.peak'),
-      size: this.element.querySelector('.peak-meter').clientWidth,
-      proxy: {
-        value: new Peak(this.get('proxyDelay'), this.get('proxyFallDuration'))
-      }
-    });
-
-    this.get('meterElements').yellow.style.left = this._formatValue(this.get('yellowLimit'));
-    this.get('meterElements').red.style.left = this._formatValue(this.get('redLimit'));
-    this.get('meterElements').proxy.peak = new Peak();
+  normalize(value) {
+    if (value > this.get('max')) {
+        return this.get('max');
+    } else if (value < this.get('min') || isNaN(value)) {
+        return this.get('min');
+    } else {
+        return (value - this.get('min') / this.get('max') - this.get('min'));
+    }
   },
 
-  _formatValue(value) {
-    return `${Math.round(value*this.get('meterElements').size)}px`
+  formatValue(value) {
+    let ratio = this.get('meterElements').size / this.get('max');
+    return `${Math.round(value*ratio)}px`
   }
  });
