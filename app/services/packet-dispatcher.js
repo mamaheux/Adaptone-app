d9fb911f5fc874a47cfe88c0ff8a14580e02edae
ddallaire <ddallaire25@gmail.com>
2019-03-12 09:36:19 -0400

    Add case indent rule

--- app/services/packet-dispatcher.js
+++ app/services/packet-dispatcher.js
@@ -7,18 +7,18 @@ export default Service.extend(Evented, {
     const message = JSON.parse(rawMessage);
 
     switch(message.seqId) {
-    case SequenceIds.ERROR_RATES:
-      this.trigger('error-rates', message.data);
-      break;
-    case SequenceIds.INPUT_SPECTRUM:
-      this.trigger('input-spectrum', message.data);
-      break;
-    case SequenceIds.CONFIRM_POS:
-      this.trigger('positions', message.data);
-      break;
-    case SequenceIds.OPTIMIZED_POS:
-      this.trigger('optimized-positions', message.data);
-      break
+      case SequenceIds.ERROR_RATES:
+        this.trigger('error-rates', message.data);
+        break;
+      case SequenceIds.INPUT_SPECTRUM:
+        this.trigger('input-spectrum', message.data);
+        break;
+      case SequenceIds.CONFIRM_POS:
+        this.trigger('positions', message.data);
+        break;
+      case SequenceIds.OPTIMIZED_POS:
+        this.trigger('optimized-positions', message.data);
+        break;
     }
   }
 });
