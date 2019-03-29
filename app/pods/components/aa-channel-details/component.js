0fd4abdb5ab5fb288def3f467a383e86da34327f
Andy <andy1337@live.ca>
2019-03-29 13:36:32 -0400

    Remove test data

--- app/pods/components/aa-channel-details/component.js
+++ app/pods/components/aa-channel-details/component.js
@@ -12,44 +12,6 @@ export default Component.extend({
     return this.channel.data.graphEq;
   }),
 
-  init() {
-    this._super(...arguments);
-    this.set('channel', {
-      seqId: 10,
-      data: {
-        channelId: 1,
-        channelName: "Master",
-        gain: 75,
-        volume: 100,
-        isMuted: false,
-        isSolo: false,
-        paramEq: [
-          {
-            id: 0,
-            on: true,
-            freq:  1000,
-            q: 4.4,
-            gain: 20
-          }
-        ],
-        graphEq: [
-          {
-            id: 0,
-            value: -10
-          },
-          {
-            id: 1,
-            value: 2
-          },
-          {
-            id: 2,
-            value: 11
-          }
-        ]
-      }
-    });
-  },
-
   actions: {
     onVolumeChange(value) {
       return value;
