a5c2e9fa6f5d055dfe1e02d58030e92124127caf
Andy <andy1337@live.ca>
2019-04-12 15:53:46 -0400

    Add ugly fake data

--- app/pods/components/aa-channel-details/component.js
+++ app/pods/components/aa-channel-details/component.js
@@ -15,6 +15,77 @@ export default Component.extend({
     this._super(...arguments);
 
     this.set('graphicEqGraphValues', {});
+
+    this.set('channel', {
+      seqId: 10,
+      data: {
+        channelId: 1,
+        channelName: "Master",
+        gain: 75,
+        volume: 100,
+        isMuted: false,
+        isSolo: false,
+        paramEq: [
+          {
+            id: 0,
+            on: true,
+            freq:  100,
+            q: 1,
+            gain: -10
+          },
+          {
+            id: 1,
+            on: true,
+            freq:  300,
+            q: 5,
+            gain: 5
+          },
+          {
+            id: 2,
+            on: true,
+            freq:  800,
+            q: 5,
+            gain: -8
+          },
+          {
+            id: 3,
+            on: true,
+            freq: 1500,
+            q: 5,
+            gain: 12
+          },
+          {
+            id: 4,
+            on: true,
+            freq: 8000,
+            q: 1,
+            gain: 2
+          }
+        ],
+        graphEq: [
+          {
+            id: 0,
+            value: -3
+          },
+          {
+            id: 0,
+            value: 3
+          },
+          {
+            id: 0,
+            value: 6
+          },
+          {
+            id: 0,
+            value: 8
+          },
+          {
+            id: 0,
+            value: 10
+          }
+        ]
+      }
+    });
   },
 
   actions: {
--- app/pods/components/aa-console/component.js
+++ app/pods/components/aa-console/component.js
@@ -4,6 +4,118 @@ export default Component.extend({
   channels: null,
   positions: null,
 
+  init() {
+    this._super(...arguments);
+
+    this.set('channels', [
+      {
+        seqId: 10,
+        data: {
+          channelId: 1,
+          channelName: "Master",
+          gain: 75,
+          volume: 100,
+          isMuted: false,
+          isSolo: false,
+          paramEq: [
+            {
+              id: 0,
+              on: true,
+              freq:  1000,
+              q: 4.4,
+              gain: 20
+            }
+          ],
+          graphEq: [
+            {
+              id: 0,
+              value: 50
+            }
+          ]
+        }
+      },
+      {
+        seqId: 10,
+        data: {
+          channelId: 2,
+          channelName: "Bass",
+          gain: 60,
+          volume: 80,
+          isMuted: false,
+          isSolo: false,
+          paramEq: [
+            {
+              id: 0,
+              on: true,
+              freq:  1000,
+              q: 4.4,
+              gain: 20
+            }
+          ],
+          graphEq: [
+            {
+              id: 0,
+              value: 50
+            }
+          ]
+        }
+      }
+    ]);
+
+    this.set('positions', {
+      seqId: 11,
+      data: {
+        positions: [
+          {
+            x: 5,
+            y: 5,
+            type: 's'
+          },
+          {
+            x: 90,
+            y: 5,
+            type: 's'
+          },
+          {
+            x: 45,
+            y: 10,
+            type: 's'
+          },
+          {
+            x: 5,
+            y: 10,
+            type: 'm',
+            errorRate: 0.12
+          },
+          {
+            x: 10,
+            y: 15,
+            type: 'm',
+            errorRate: 0.09
+          },
+          {
+            x: 30,
+            y: 30,
+            type: 'm',
+            errorRate: 0.07
+          },
+          {
+            x: 50,
+            y: 15,
+            type: 'm',
+            errorRate: 0.09
+          },
+          {
+            x: 80,
+            y: 30,
+            type: 'm',
+            errorRate: 0.13
+          }
+        ]
+      }
+    });
+  },
+
   actions: {
     onChannelVolumeChange(channel) {
       // Handle channel volume change here
