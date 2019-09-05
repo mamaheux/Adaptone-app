b8c7381ecb2585f4d3c7e799a2c66437d029d86b
Andy Larochelle <dylarochelle@gmail.com>
2019-09-05 13:40:52 -0400

    Display channels properly with the new structure

--- app/pods/components/aa-console/component.js
+++ app/pods/components/aa-console/component.js
@@ -9,6 +9,60 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
+
+    // TODO : Remove this but leave it in for now as it makes testing the whole app easier
+    this.set('positions', {
+      seqId: 11,
+      data: {
+        positions: [
+          {
+            x: 5,
+            y: 5,
+            type: 's'
+          },
+          {
+            x: 90,
+            y: 5,
+            type: 's'
+          },
+          {
+            x: 45,
+            y: 10,
+            type: 's'
+          },
+          {
+            x: 5,
+            y: 10,
+            type: 'm',
+            errorRate: 0.12
+          },
+          {
+            x: 10,
+            y: 15,
+            type: 'm',
+            errorRate: 0.09
+          },
+          {
+            x: 30,
+            y: 30,
+            type: 'm',
+            errorRate: 0.07
+          },
+          {
+            x: 50,
+            y: 15,
+            type: 'm',
+            errorRate: 0.09
+          },
+          {
+            x: 80,
+            y: 30,
+            type: 'm',
+            errorRate: 0.13
+          }
+        ]
+      }
+    });
   },
 
   actions: {
--- app/pods/console/route.js
+++ app/pods/console/route.js
@@ -1,4 +1,445 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(params) {
+      if (this.controllerFor('console').get('channels') === null) {
+        const masterInputs = [
+        {
+          seqId: 10,
+          data: {
+            channelId: 2,
+            portId: 0,
+            channelName: 'Input 1',
+            gain: 3.00,
+            isMuted: false,
+            isSolo: false,
+            paramEq: [
+              {
+                id: 0,
+                on: true,
+                freq: 100,
+                q: 1,
+                gain: 0
+              },
+              {
+                id: 1,
+                on: true,
+                freq: 300,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 2,
+                on: true,
+                freq: 800,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 3,
+                on: true,
+                freq: 1500,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 4,
+                on: true,
+                freq: 8000,
+                q: 1,
+                gain: 0
+              }
+            ],
+            graphEq: [
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              }
+            ]
+          }
+        },
+        {
+          seqId: 10,
+          data: {
+            channelId: 3,
+            portId: 0,
+            channelName: 'Input 2',
+            gain: 3.00,
+            isMuted: false,
+            isSolo: false,
+            paramEq: [
+              {
+                id: 0,
+                on: true,
+                freq: 100,
+                q: 1,
+                gain: 0
+              },
+              {
+                id: 1,
+                on: true,
+                freq: 300,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 2,
+                on: true,
+                freq: 800,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 3,
+                on: true,
+                freq: 1500,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 4,
+                on: true,
+                freq: 8000,
+                q: 1,
+                gain: 0
+              }
+            ],
+            graphEq: [
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              }
+            ]
+          }
+        }];
+
+        const auxInputs = [
+        {
+          seqId: 10,
+          portId: 1,
+          data: {
+            channelId: 4,
+            portId: 1,
+            channelName: 'Input 3',
+            gain: 3.00,
+            isMuted: false,
+            isSolo: false,
+            paramEq: [
+              {
+                id: 0,
+                on: true,
+                freq: 100,
+                q: 1,
+                gain: 0
+              },
+              {
+                id: 1,
+                on: true,
+                freq: 300,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 2,
+                on: true,
+                freq: 800,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 3,
+                on: true,
+                freq: 1500,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 4,
+                on: true,
+                freq: 8000,
+                q: 1,
+                gain: 0
+              }
+            ],
+            graphEq: [
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              }
+            ]
+          }
+        },
+        {
+          seqId: 10,
+          data: {
+            channelId: 5,
+            portId: 1,
+            channelName: 'Input 4',
+            gain: 3.00,
+            isMuted: false,
+            isSolo: false,
+            paramEq: [
+              {
+                id: 0,
+                on: true,
+                freq: 100,
+                q: 1,
+                gain: 0
+              },
+              {
+                id: 1,
+                on: true,
+                freq: 300,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 2,
+                on: true,
+                freq: 800,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 3,
+                on: true,
+                freq: 1500,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 4,
+                on: true,
+                freq: 8000,
+                q: 1,
+                gain: 0
+              }
+            ],
+            graphEq: [
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              }
+            ]
+          }
+        }];
+
+        const master = {
+          seqId: 10,
+          data: {
+            channelId: 0,
+            channelName: 'Master',
+            gain: 3.00,
+            volume: 50,
+            isMuted: false,
+            isSolo: false,
+            inputs: masterInputs,
+            paramEq: [
+              {
+                id: 0,
+                on: true,
+                freq: 100,
+                q: 1,
+                gain: 0
+              },
+              {
+                id: 1,
+                on: true,
+                freq: 300,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 2,
+                on: true,
+                freq: 800,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 3,
+                on: true,
+                freq: 1500,
+                q: 5,
+                gain: 0
+              },
+              {
+                id: 4,
+                on: true,
+                freq: 8000,
+                q: 1,
+                gain: 0
+              }
+            ],
+            graphEq: [
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              },
+              {
+                id: 0,
+                value: 0
+              }
+            ]
+          }
+        };
+
+        const auxiliaries = [
+          {
+            seqId: 10,
+            data: {
+              channelId: 1,
+              channelName: 'Aux 1',
+              gain: 3.00,
+              volume: 50,
+              isMuted: false,
+              isSolo: false,
+              inputs: auxInputs,
+              paramEq: [
+                {
+                  id: 0,
+                  on: true,
+                  freq: 100,
+                  q: 1,
+                  gain: 0
+                },
+                {
+                  id: 1,
+                  on: true,
+                  freq: 300,
+                  q: 5,
+                  gain: 0
+                },
+                {
+                  id: 2,
+                  on: true,
+                  freq: 800,
+                  q: 5,
+                  gain: 0
+                },
+                {
+                  id: 3,
+                  on: true,
+                  freq: 1500,
+                  q: 5,
+                  gain: 0
+                },
+                {
+                  id: 4,
+                  on: true,
+                  freq: 8000,
+                  q: 1,
+                  gain: 0
+                }
+              ],
+              graphEq: [
+                {
+                  id: 0,
+                  value: 0
+                },
+                {
+                  id: 0,
+                  value: 0
+                },
+                {
+                  id: 0,
+                  value: 0
+                },
+                {
+                  id: 0,
+                  value: 0
+                },
+                {
+                  id: 0,
+                  value: 0
+                }
+              ]
+            }
+          }
+        ]
+
+        this.controllerFor('console').set('channels', {
+          master,
+          auxiliaries
+        });
+
+        return params;
+      }
+    }
 });
