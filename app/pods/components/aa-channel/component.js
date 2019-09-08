5a33fe351cc7bdabe84b541ebbbf710a39f34fda
ddallaire <ddallaire25@gmail.com>
2019-09-08 16:27:01 -0400

    Edit channel name

--- app/pods/components/aa-channel/component.js
+++ app/pods/components/aa-channel/component.js
@@ -1,8 +1,21 @@
 import Component from '@ember/component';
 import {computed} from '@ember/object';
+import {run} from '@ember/runloop';
+import $ from 'jquery';
 
 export default Component.extend({
   stringifiedChannel: computed('channel', function() {
     return JSON.stringify(this.channel);
-  })
+  }),
+
+  didRender() {
+    this._super(...arguments);
+
+    $('.input').on('input', () => {
+      run(() => {
+        let newValue = $('.input').text();
+        this.set('channel.data.channelName', newValue);
+      });
+    });
+  }
 });
