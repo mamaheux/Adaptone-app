f827b48d8e9946717ff5912817bbb1b2c45ceb7a
Andy Larochelle <dylarochelle@gmail.com>
2019-09-09 09:13:39 -0400

    Handle volume change on console view

--- app/pods/components/aa-channel/component.js
+++ app/pods/components/aa-channel/component.js
@@ -1,8 +1,57 @@
 import Component from '@ember/component';
-import {computed} from '@ember/object';
+import {computed, observer} from '@ember/object';
+import {inject as service} from '@ember/service';
+import {debounce} from '@ember/runloop';
+
+import SequenceIds from 'adaptone-front/constants/sequence-ids';
+
+const DEBOUNCE_TIME = 20;
 
 export default Component.extend({
+  connection: service('connection'),
+
+  isAuxiliary: computed('channel.data.auxiliaryId', function() {
+    if (this.get('channel').data.auxiliaryId !== null) return true;
+
+    return false;
+  }),
+
+  isAuxiliaryOutput: computed('channel.data.{auxiliaryId,inputs}', function() {
+    if (this.get('channel').data.auxiliaryId !== null && this.get('channel').data.inputs !== undefined) return true;
+
+    return false;
+  }),
+
+  isMasterOutput: computed('channel.data.{auxiliaryId,inputs}', function() {
+    if (this.get('channel').data.auxiliaryId === null && this.get('channel').data.inputs !== undefined) return true;
+
+    return false;
+  }),
+
+  channelVolumeChanged: observer('channel.data.volume', function() {
+      const seqId = this._getVolumeSequenceId();
+
+      const message = {
+        seqId,
+        data: {
+          auxiliaryId: this.get('channel').data.auxiliaryId,
+          channelId: this.get('channel').data.channelId,
+          gain: this.get('channel').data.volume
+        }
+      };
+
+      debounce(this.get('connection'), this.get('connection').sendMessage, message, DEBOUNCE_TIME);
+  }),
+
   stringifiedChannel: computed('channel', function() {
     return JSON.stringify(this.channel);
-  })
+  }),
+
+  _getVolumeSequenceId() {
+    if (this.get('isAuxiliaryOutput')) return SequenceIds.CHANGE_AUX_VOLUME_OUTPUT;
+    if (this.get('isMasterOutput')) return SequenceIds.CHANGE_MAIN_VOLUME_OUTPUT;
+    if (this.get('isAuxiliary')) return SequenceIds.CHANGE_AUX_VOLUME_INPUT;
+
+    return SequenceIds.CHANGE_MAIN_VOLUME_INPUT;
+  }
 });
