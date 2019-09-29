e3376e693131c280b569e74d19c3817df54d9af7
ddallaire <ddallaire25@gmail.com>
2019-09-29 16:53:10 -0400

    Remove ui-slider

--- app/pods/components/aa-slider/component.js
+++ app/pods/components/aa-slider/component.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
+import {htmlSafe} from '@ember/template';
 
 export default Component.extend({
+  rangeStyle: computed('min', 'max', 'value', function() {
+    const {min, max, value} = this.getProperties('min', 'max', 'value');
+    return htmlSafe(`--min: ${min}; --max: ${max}; --val: ${value}`);
+  }),
+
+  input(e) {
+    this.set('value', e.target.value);
+  }
 });
