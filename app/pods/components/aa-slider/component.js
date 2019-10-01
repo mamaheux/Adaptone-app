210bf34cca1c5f0f7a672f4b6a55d3594990c00c
David Dallaire <ddallaire25@gmail.com>
2019-10-01 12:26:25 -0400

    Slider change (#81)
    * Remove ui-slider
    
    * Fix slider events
    
    * Fix onGainChange
    
    * Fix slider thumb color in outputs

--- app/pods/components/aa-slider/component.js
+++ app/pods/components/aa-slider/component.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
+import {htmlSafe} from '@ember/template';
 
 export default Component.extend({
+  rangeStyle: computed('min', 'max', 'value', function() {
+    const {min, max, value} = this.getProperties('min', 'max', 'value');
+    return htmlSafe(`--min: ${min}; --max: ${max}; --val: ${value}`);
+  }),
+
+  actions: {
+    updateValue(value) {
+      this.set('value', value);
+    }
+  }
 });
